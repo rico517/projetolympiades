b8ca9e8cfd326c86b0b3b0103887a73ed9aef78a
rico517 <delasalle.sio.girard.e@gmail.com>
2024-03-15 09:45:13 +0100

    ajout test connection db

---
+++ src/data/test_co_db.js
@@ -0,0 +1,18 @@
+import mysql from 'mysql';
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "projetolympiadls"
+
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query('select nom from equipes', function (err, result) {
+      if (err) throw err;
+      console.log("Result: " + result);
+    });
+});
