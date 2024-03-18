5e928877d17a027664388107f06d3ed7272d5e6b
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2024-03-18 16:20:44 +0100

    Création de classes au format .js

---
+++ back_end/classes/Section.js
@@ -0,0 +1,33 @@
+/*
+Autheur : Julien Demogue
+*/
+
+export class Section{
+    // Constructeur de la classe
+    constructor(id,nom,scoreTotal,couleur){
+        this._id = id;
+        this._nom = nom;
+        this._scoreTotal = scoreTotal;
+        this._couleur = couleur;
+    }
+
+    // Recuperer l'id
+    get id(){return this.id;}
+    // Modifier l'id
+    set id(nouveauId){this.id = nouveauId;}
+
+    // Recuperer le nom
+    get nom(){return this.nom;}
+    // Modifier le nom
+    set nom(nouveauNom){this.nom = nouveauNom;}
+
+    // Recuperer le score total
+    get scoreTotal(){return this.scoreTotal;}
+    // Modifier le score total
+    set scoreTotal(nouveauScoreTotal){this.scoreTotal = nouveauScoreTotal;}
+    
+    // Recuperer la couleur
+    get couleur(){return this.couleur;}
+    // Modifier la couleur
+    set couleur(nouveauCouleur){this.couleur = nouveauCouleur;}
+}
