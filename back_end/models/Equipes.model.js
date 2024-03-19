59b3e892db0d152efe3d0db5ae15328edfa0e131
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2024-03-19 13:08:05 +0100

    Déplacement des classes coté front-end

--- back_end/models/Equipes.model.js
+++ back_end/models/Equipes.model.js
@@ -1,5 +1,4 @@
 import sql from "../data/Connection.js";
-import Equipe from "../classes/Equipe.class.js";
 
 export function getToutesLesEquipes(res) {
     sql.query(`SELECT * FROM equipes`, (err, result) => {
--- back_end/models/Jeux.model.js
+++ back_end/models/Jeux.model.js
@@ -4,7 +4,6 @@ auteur : Julien Demogue
 */
 
 import sql from "../data/Connection.js";
-import Jeu from '../classes/Jeu.class.js';
 
 /*
 Renvoie un jeu a partir d'un ID
@@ -17,13 +16,12 @@ export function getUnJeu(id,res) {
         // Cas d'erreur dans l'execution de la requete
         if (err) {
             console.log("erreur dans la requete : ", err);
-            res(null,err);
+            res(err,null);
             return;
         }
         // Si aucun resultat n'est trouve
         else if (result.length === 0) {
             console.log("Le jeu d'id " + id + " n'existe pas");
-            //res (null);
         }
         else{
         // Si un resultat est trouve, envoyer un objet de type Jeu avec les parametres du resultat
