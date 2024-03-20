86c760fbc024e56581027e4098a41fedc478ae4d
Julien Demogue <julien.demogue@sfr.fr>
2024-03-20 09:00:19 +0100

    Ajout des methodes dans la passerelleJson

--- front_end/src/services/PasserelleJson.services.js
+++ front_end/src/services/PasserelleJson.services.js
@@ -2,7 +2,39 @@ import http from "../Http-common.js";
 
 class DataService{
     getToutesLesEquipes(){
-        return http.get("/equipes");
+        return http.get("/GetTouteLesEquipes");
+    }
+
+    getTouteLesSections(){
+        return http.get("/GetTouteLesSections");
+    }
+
+    getTousLesJeux(){
+        return http.get("/GetTousLesJeux");
+    }
+
+    getTousLesPlannings(){
+        return http.get("/GetTousLesPlannings");
+    }
+
+    getUnJeu(id){
+        return http.get("/GetUnJeu/" + id);
+    }
+
+    getUnPlanning(id){
+        return http.get("/GetUnPlanning/" + id);
+    }
+
+    getUneSection(id){
+        return http.get("/GetUneSection/" + id);
+    }
+
+    getUnUtilisateur(identifiant, mdp){
+        return http.get("/GetUnUtilisateur/" + identifiant + "/" + mdp);
+    }
+
+    getEquipesPlanning(idJeu){
+        return http.get("/GetEquipesPlanning/" + idJeu);
     }
 }
 
