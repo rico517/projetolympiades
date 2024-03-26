ca1c8c3bd85e08415524e3e9f173f0bf6264d05d
Julien Demogue <82449121+Julien-Demogue@users.noreply.github.com>
2024-03-26 17:17:37 +0100

    correction affichage page erreur

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
 import {RouterView } from 'vue-router'
 import HeaderComponent from './assets/components/Header.component.vue';
+import NomPages from './enums/NomPages.enum';
 </script>
 
 <template>
-  <HeaderComponent  v-if="$route.path != '/'"/>
+  <HeaderComponent  v-if="this.$route.path != '/' && this.$route.name != NomPages.erreur"/>
   <router-view/>
 </template>
