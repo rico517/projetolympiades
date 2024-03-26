e4978707290fe7c77e58f813ae276486d6efd6e0
Noé <105778037+Daiteau@users.noreply.github.com>
2024-03-26 17:28:30 +0100

    daito AdminJeuxTemplate

--- front_end/src/assets/components/AdminJeuxTemplate.component.vue
+++ front_end/src/assets/components/AdminJeuxTemplate.component.vue
@@ -5,7 +5,16 @@ import Jeu from '../../classes/Jeu.class.ts';
 
 <template>
     <div id="container">
-        {{ idJeu }} 
+        <div id="contentContainer">
+            <h1> {{ leJeu.libelle }}</h1>
+            <p id="infosHolder">
+                Points attribuables : {{ leJeu.nbPoints }} <br>
+                Type de jeu : {{ leJeu.typeJeu }}
+            </p>
+            <p id="reglesHolder">
+                Règles du jeu appelées depuis le fichier
+            </p>
+        </div>
     </div>
 </template>
 
@@ -18,12 +27,26 @@ export default {
         }
     },
     data() {
-        return {}
+        return {
+            leJeu: Jeu // Initialisez le jeu à null pour éviter des erreurs lors de son chargement
+        }
     },
     mounted() {
-
+        this.getLeJeu(this.idJeu); // Appelez la fonction getLeJeu() avec this.idJeu comme paramètre
     },
-    methods: {},
+    methods: {
+        getLeJeu(idJeu){
+            DataServices.getUnJeu(idJeu)
+            .then(response => {
+                const jeu = response.data[0]
+                // Assignez le jeu à this.leJeu pour le rendre disponible dans le template
+                this.leJeu = new Jeu(jeu.id,jeu.libelle,jeu.regles,jeu.nbPoints,jeu.typeJeu);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+        }
+    }
 }
 </script>
 
@@ -31,11 +54,54 @@ export default {
 #container {
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: start;
     background-color: white;
     height:100%;
     width:83%;
+    overflow-y: scroll;
 }
+
+    #contentContainer {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        width:90%;
+    }
+
+        #contentContainer h1{
+            padding:0.5rem 3rem 0.5rem 3rem;
+            margin: 5rem 0 5rem 0;
+            border-radius: 1rem;
+            background-color: #E7191F;
+            color:whitesmoke;
+            font-size: 5rem;
+        }
+
+    #infosHolder {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        padding:0.5rem 3rem 0.5rem 3rem;
+        margin: 5rem 0 5rem 0;
+        border-radius: 1rem;
+        font-size: 2rem;
+        box-shadow: 0 0 1rem #AAAAAA;
+        line-height: 3rem;
+    }
+
+    #reglesHolder{
+        display: flex;
+        justify-content: center;
+        width:90%;
+        margin: 5rem 0 5rem 0;
+        padding: 2rem 0 2rem 0;
+        box-shadow: 0 0 1rem #AAAAAA;
+        border-radius: 1rem;
+        font-size: 2rem;
+    }
+
+    
 
 /* 160px + 942px */
 @media all and (orientation : portrait) { 
